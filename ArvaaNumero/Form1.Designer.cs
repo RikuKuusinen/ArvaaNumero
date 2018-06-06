045d89a6bba06f2f2b7ae16d810e05183e20d1bc
Riku Kuusinen <riku.kuusinen91@gmail.com>
2018-06-06 15:22:12 +0300

    Add project files.

---
+++ ArvaaNumero/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ArvaaNumero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.arvauksiaJaljella = new System.Windows.Forms.Label();
+            this.arvaus = new System.Windows.Forms.TextBox();
+            this.arvaa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // arvauksiaJaljella
+            // 
+            this.arvauksiaJaljella.AutoSize = true;
+            this.arvauksiaJaljella.Location = new System.Drawing.Point(13, 13);
+            this.arvauksiaJaljella.Name = "arvauksiaJaljella";
+            this.arvauksiaJaljella.Size = new System.Drawing.Size(129, 17);
+            this.arvauksiaJaljella.TabIndex = 0;
+            this.arvauksiaJaljella.Text = "Arvauksia jäljellä: 0";
+            // 
+            // arvaus
+            // 
+            this.arvaus.Location = new System.Drawing.Point(13, 34);
+            this.arvaus.Name = "arvaus";
+            this.arvaus.Size = new System.Drawing.Size(100, 22);
+            this.arvaus.TabIndex = 1;
+            // 
+            // arvaa
+            // 
+            this.arvaa.Location = new System.Drawing.Point(119, 34);
+            this.arvaa.Name = "arvaa";
+            this.arvaa.Size = new System.Drawing.Size(75, 23);
+            this.arvaa.TabIndex = 2;
+            this.arvaa.Text = "Arvaa";
+            this.arvaa.UseVisualStyleBackColor = true;
+            this.arvaa.Click += new System.EventHandler(this.arvaa_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 185);
+            this.Controls.Add(this.arvaa);
+            this.Controls.Add(this.arvaus);
+            this.Controls.Add(this.arvauksiaJaljella);
+            this.Name = "Form1";
+            this.Text = "Arvaa Numero";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label arvauksiaJaljella;
+        private System.Windows.Forms.TextBox arvaus;
+        private System.Windows.Forms.Button arvaa;
+    }
+}
+
---
+++ ArvaaNumero/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArvaaNumero
+{
+    public partial class Form1 : Form
+    {
+        public int numero;
+        public int arvauksia;
+        public Random rng;
+     
+        public Form1()
+        {
+            InitializeComponent();
+            Random rng = new Random();
+            numero = rng.Next(10) + 1;
+            alusta();
+
+        }
+
+        private void arvaa_Click_1(object sender, EventArgs e)
+        {
+            int arvattuNumero;
+            if (int.TryParse(arvaus.Text, out arvattuNumero) && (arvattuNumero >= 1 && arvattuNumero <= 10))
+            {
+                if (arvattuNumero == numero)
+                { 
+                    MessageBox.Show("Onneksi olkoon!\nArvasit oikein!");
+                alusta();
+            }
+            else if (arvattuNumero > numero)
+            {
+                arvauksia--;
+                MessageBox.Show("Arvaamasi numero oli liian suuri!");
+            }
+            else if (arvattuNumero < numero)
+            {
+                arvauksia--;
+                MessageBox.Show("Arvaamasi numero oli liian pieni!");
+            }
+            arvauksiaJaljella.Text = "Arvauksia jäljellä: " + arvauksia;
+
+            if (arvauksia == 0)
+            {
+                MessageBox.Show("Arvauksesi loppuivat kesken");
+                alusta();
+            }
+        }
+            else
+            {
+                MessageBox.Show("Arvauksen tulee olla numero 1-10!");
+            }
+
+        }
+        private void alusta()
+        {
+        
+            arvauksia = 3;
+            arvauksiaJaljella.Text = "Arvauksia jäljellä: " + arvauksia;
+
+        }
+
+    }
+}
